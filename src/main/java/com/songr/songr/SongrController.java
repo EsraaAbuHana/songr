a2b728c2e8307c951c4bc7105bf769b33903f280
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-04-18 16:43:40 +0300

    func done

---
+++ src/main/java/com/songr/songr/SongrController.java
@@ -0,0 +1,41 @@
+package com.songr.songr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@Controller
+public class SongrController {
+    @GetMapping("/hello")
+//    @ResponseBody
+    public String home() {
+        return "HelloWorld";
+    }
+    @GetMapping("/capitalize/{word}")
+    public String anyWord(Model m, @PathVariable("word")String word){
+        m.addAttribute("word",word.toUpperCase());
+
+        return "CAPITALIZED.html";
+    }
+    @GetMapping("/albums")
+    public String getAlbum(Model m){
+
+        Album album1 = new Album("SKY","Yiruma",8,500,"https://img.discogs.com/DgLWcqi0mMWsayw856ptriTN9qc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6875064-1428500220-1592.jpeg.jpg");
+        Album album2 = new Album("SKY","Yiruma",8,500,"https://img.discogs.com/DgLWcqi0mMWsayw856ptriTN9qc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6875064-1428500220-1592.jpeg.jpg");
+        Album album3 = new Album("SKY","Yiruma",8,500,"https://img.discogs.com/DgLWcqi0mMWsayw856ptriTN9qc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6875064-1428500220-1592.jpeg.jpg");
+
+        List<Album> albums = new ArrayList<>();
+        albums.add(album1);
+        albums.add(album2);
+        albums.add(album3);
+
+        m.addAttribute("albums",albums);
+        return "albums.html";
+    }
+}
--- src/test/java/com/songr/songr/SongrApplicationTests.java
+++ src/test/java/com/songr/songr/SongrApplicationTests.java
@@ -2,6 +2,8 @@ package com.songr.songr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+		import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class SongrApplicationTests {
@@ -9,5 +11,34 @@ class SongrApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+@Test
+	void AlbumTest(){
+	Album testAlbum = new Album("SKY","Yiruma",8,500,"https://img.discogs.com/DgLWcqi0mMWsayw856ptriTN9qc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6875064-1428500220-1592.jpeg.jpg");
+	assertThat(testAlbum.getTitle()).isEqualTo("SKY");
+	assertThat(testAlbum.getArtist()).isEqualTo("Yiruma");
+	assertThat(testAlbum.getSongCount()).isEqualTo(8);
+	assertThat(testAlbum.getLength()).isEqualTo(500);
+	assertThat(testAlbum.getImageUrl()).isEqualTo("https://img.discogs.com/DgLWcqi0mMWsayw856ptriTN9qc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6875064-1428500220-1592.jpeg.jpg");
 
+}
+@Test
+	void setterAlbumsTest(){
+		final Album album = new Album();
+	final Album album1 = new Album();
+	album.setArtist("person");
+	final Album field = album.getClass().getDeclaredField("value");
+        field.setAccessible(true);
+	assertEquals("Fields didn't match", field.get(album), "person");
+
+}
+
+//	final PlainOldJavaObject pojo = new PlainOldJavaObject();
+//
+//	//when
+//        pojo.setValue("foo");
+//
+//	//then
+//	final Field field = pojo.getClass().getDeclaredField("value");
+//        field.setAccessible(true);
+//	assertEquals("Fields didn't match", field.get(pojo), "foo");
 }
